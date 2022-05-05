4f1afed0530109a51757a591b3fe16363721fe60
iwonaive <98330863+iwonaive@users.noreply.github.com>
2022-05-05 22:20:41 +0200

    Added another steps to forecast in js

--- src/app.js
+++ src/app.js
@@ -21,9 +21,10 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
-  
+
   let days = ["Thu", "Fri", "Sat", "Sun"];
   let forecastHTML = `<div class="row">`;
  
@@ -38,12 +39,20 @@ function displayForecast() {
                         <div class="weather-forecast-temperatures">
                         <span class="weather-forecast-temperature-max">18°C</span>
                         <span class="weather-forecast-temperature-min">10°C</span>
-                    
+                    </div>
                 </div>`
+  } );
   forecastHTML = forecastHTML + `</div>`;
 
   forecastElement.innerHTML = forecastHTML;
-  } ) }; 
+  }
+  
+  function getForecast(coordinates) {
+    console.log(coordinates);
+    let apiKey = "e4ed7896f0b74bd1c1058cf4259ba869";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
+  }
 
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
@@ -67,6 +76,8 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -94,5 +105,3 @@ let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
 search("Warsaw");
-
-displayForecast();
