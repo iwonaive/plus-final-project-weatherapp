8887034a50d11366f0392212f1ec7c61d98688cf
iwonaive <98330863+iwonaive@users.noreply.github.com>
2022-05-05 23:31:30 +0200

    finished

--- src/app.js
+++ src/app.js
@@ -50,7 +50,7 @@ function displayForecast(response) {
                         <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="50" />
                         <div class="weather-forecast-temperatures">
                         <span class="weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)}°C</span>
-                        <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}°C</span>
+                        
                     </div>
                 </div>`
   } } );
