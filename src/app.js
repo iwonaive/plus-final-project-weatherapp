9e8326d7490bc603a0d545c6190611293450c594
iwonaive <98330863+iwonaive@users.noreply.github.com>
2022-05-05 23:13:34 +0200

    Almost finished

--- src/app.js
+++ src/app.js
@@ -40,14 +40,14 @@ function displayForecast(response) {
   let forecastHTML = `<div class="row">`;
  
   forecast.forEach(function (forecastDay, index) {
-    if (index < 6) {
+    if (index < 7) if (index > 0) {
 
   forecastHTML = 
   forecastHTML + 
   `
               <div class="col-2">
                         <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
-                        <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="36" />
+                        <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="50" />
                         <div class="weather-forecast-temperatures">
                         <span class="weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)}°C</span>
                         <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}°C</span>
