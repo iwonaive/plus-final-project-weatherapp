adec64e23bd98d71a386be5c63c1384682148cd0
iwonaive <98330863+iwonaive@users.noreply.github.com>
2022-05-05 21:55:43 +0200

    Added the preview of forecast

--- src/app.js
+++ src/app.js
@@ -21,6 +21,30 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  let forecastHTML = `<div class="row">`;
+ 
+  days.forEach(function (day) {
+
+  forecastHTML = 
+  forecastHTML + 
+  `
+              <div class="col-2">
+                        <div class="weather-forecast-date">${day}</div>
+                        <img src="http://openweathermap.org/img/wn/50d@2x.png" alt="" width="36" />
+                        <div class="weather-forecast-temperatures">
+                        <span class="weather-forecast-temperature-max">18°C</span>
+                        <span class="weather-forecast-temperature-min">10°C</span>
+                    
+                </div>`
+  forecastHTML = forecastHTML + `</div>`;
+
+  forecastElement.innerHTML = forecastHTML;
+  } ) }; 
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -70,3 +94,5 @@ let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
 search("Warsaw");
+
+displayForecast();
